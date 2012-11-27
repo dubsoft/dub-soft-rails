d90c11554844c3d3b11a8205351d74f7350403d9
wlaurance <w.laurance@gmail.com>
2012-11-26 21:40:27 -0500

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DubSoftRails::Application.config.secret_token = '04d13db80cdd51640a5648f36f7493f48fead87b8a6b78ff55de4445ca40f8ffdef9e11fb8f675304dc07b940ab149d4ad2fc96d0a46528679133938a9ebee3d'
